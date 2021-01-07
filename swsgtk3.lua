4805a7a596f2bf291893eb52a694352c31e29027
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2021-01-07 05:16:04 -0400

    builder:get_object => ui

--- swsgtk3.lua
+++ swsgtk3.lua
@@ -2,13 +2,12 @@
 --[[!
  @package   Simple Web Server gtk3
  @filename  swsgtk3.lua
- @version   1.3
+ @version   2.0
  @autor     Diaz Urbaneja Victor Eduardo Diex    <diaz.victor@openmailbox.org> 2018
  @autor     Díaz Urbaneja Víctor Diex Gamar      <Sirkennov@outlook.com> 2018
- @autor     Diaz Urbaneja Victor Diego Alejandro <sodomon2@gmail.com> 2020
+ @autor     Diaz Urbaneja Victor Diego Alejandro <sodomon2@gmail.com> 2020-2021
  @date      29.04.2020 17:55:00 -04
 ]]--
-
 
 local lgi     = require 'lgi'               -- La libreria que me permitira usar GTK
 local GObject = lgi.GObject                 -- Parte de lgi
@@ -20,55 +19,42 @@ local builder = Gtk.Builder()
 
 assert(builder:add_from_file('simple_web_server.ui'))
 local ui = builder.objects
-local window_main = ui.window_main  --la vetana del main
-
 local app_run = false
 
 --que hacer cuando le den cerrar a la ventana del login
-function window_main:on_destroy()
+function ui.window_main:on_destroy()
 	Gtk.main_quit()
 	os.execute('killall -9 simple_web_server')
 end
 
-local compartir = builder:get_object('compartir')       -- este seria el boton de compartir
-local info      = builder:get_object('info')            -- este seria el label de informacion
-local about     = builder:get_object('about')           -- este seria el boton de about
-local selec     = builder:get_object('selec')           -- este seria el input de seleccionar directorio
-local port      = builder:get_object('port')            -- este seria el input de port
-
-local input_select = builder:get_object('input_select') -- este seria el selector de directorios
-
-
-function compartir:on_clicked()
+function ui.compartir:on_clicked()
 	condition = true
 	numero = 0
 	
-	print(selec:get_filename(), port.text, input_select:get_active_id())
-	local pwd = selec:get_filename()
+	print(ui.selec:get_filename(), ui.port.text, ui.input_select:get_active_id())
+	local pwd = ui.selec:get_filename()
 	if pwd and app_run == false then
-		info.label = "Ejecutando"
-		local cmd = "/usr/bin/simple_web_server -p  ".. port.text .." -r  ".. pwd .."  &"
+		ui.info.label = "Running"
+		local cmd = "/usr/bin/simple_web_server -p  ".. ui.port.text .." -r  ".. pwd .."  &"
 		os.execute(cmd)
 		app_run = true
 		
 	else
-		info.label = "Sin ejecutar"
+		ui.info.label = "Error"
 	end
 end
 
-local cancel = builder:get_object('cancel') --este seria el boton de cancelar
-
-function cancel:on_clicked()
+function ui.cancel:on_clicked()
 	os.execute('killall -9 simple_web_server')
-	info.label = ""
+	ui.info.label = ""
 	app_run = false
 
 end  
 
-function about:on_clicked()
+function ui.about:on_clicked()
 	ui.window_about:run()
 	ui.window_about:hide()
-end  
+end
 
-window_main:show_all()
+ui.window_main:show_all()
 Gtk.main()
